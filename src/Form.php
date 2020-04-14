b7a7af32a55de363b91b4b5a06e86489fb334dc4
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-14 15:09:30 +0300

    feat: include only optional form fields that has value

--- src/Form.php
+++ src/Form.php
@@ -8,19 +8,30 @@ class Form
 {
     const DEFAULT_FORM_ID = 'PaytrailPaymentForm';
 
-    public static function create(array $paymentData, string $formId): string
+    public static function createPaymentWidget(array $paymentData, string $buttonText, string $formId, ?string $widgetUrl): string
     {
-        $formData = "<form id=\"{$formId}\">";
+        $formData = "<form action=\"https://payment.paytrail.com/e2\" id=\"{$formId}\">";
         foreach ($paymentData as $key => $value) {
             $formData .= "<input name=\"{$key}\" type=\"hidden\" value=\"{$value}\">\n";
         }
 
-        $formData .= '<input type="submit" value="Pay here"></form>
-            <script type="text/javascript" src="//payment.paytrail.com/js/payment-widget-v1.0.min.js"></script>
-            <script type="text/javascript">';
+        $widgetUrl = $widgetUrl ?? self::WIDGET_URL;
+
+        $formData .= "<input type=\"submit\" value=\"{$buttonText}\"></form><script type=\"text/javascript\" src=\"{$widgetUrl}\">
+        </script><script type=\"text/javascript\">SV.widget.initWithForm('{$formId}', {charset:'UTF-8'});</script>";
+
+        return $formData;
+    }
+
+    public static function createPaymentForm(array $paymentData, string $buttonText, string $formId): string
+    {
+        $formData = "<form action=\"https://payment.paytrail.com/e2\"  id=\"{$formId}\">";
+        foreach ($paymentData as $key => $value) {
+            $formData .= "<input name=\"{$key}\" type=\"hidden\" value=\"{$value}\">\n";
+        }
 
         $formData .= "SV.widget.initWithForm('{$formId}', {charset:'UTF-8'});</script>";
-    
+
 
         return $formData;
     }
