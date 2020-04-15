3ee9a5f7a48be3ad127de6b4d3e22ab9ef94c72b
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-15 13:17:08 +0300

    doc: add dockblocks to methods

--- src/Customer.php
+++ src/Customer.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Paytrail\E2Module;
 
+/**
+ * Customer class for holding customer details for payment.
+ *
+ * @package E2-Module
+ * @author Paytrail <tech@paytrail.com>
+ */
 class Customer
 {
         public $PAYER_PERSON_FIRSTNAME;
@@ -16,16 +22,22 @@ class Customer
         public $PAYER_PERSON_PHONE;
         public $PAYER_COMPANY_NAME;
 
-        public static function create(array $customerData)
+        /**
+         * Create customer for payment.
+         *
+         * @param array $customerData
+         * @return void
+         */
+        public static function create(array $customerData): self
         {
                 $customer = new Self();
-                $customer->PAYER_PERSON_FIRSTNAME = $customerData['PAYER_PERSON_FIRSTNAME'];
-                $customer->PAYER_PERSON_LASTNAME = $customerData['PAYER_PERSON_LASTNAME'];
+                $customer->PAYER_PERSON_FIRSTNAME = $customerData['PAYER_PERSON_FIRSTNAME'] ?? null;
+                $customer->PAYER_PERSON_LASTNAME = $customerData['PAYER_PERSON_LASTNAME'] ?? null;
                 $customer->PAYER_PERSON_EMAIL = $customerData['PAYER_PERSON_EMAIL'] ?? null;
                 $customer->PAYER_PERSON_ADDR_STREET = $customerData['PAYER_PERSON_ADDR_STREET'] ?? null;
                 $customer->PAYER_PERSON_ADDR_POSTAL_CODE = $customerData['PAYER_PERSON_ADDR_POSTAL_CODE'] ?? null;
                 $customer->PAYER_PERSON_ADDR_TOWN = $customerData['PAYER_PERSON_ADDR_TOWN'] ?? null;
-                $customer->PAYER_PERSON_ADDR_COUNTRY = $customerData['PAYER_PERSON_ADDR_COUNTRY'] ?? 'FI';
+                $customer->PAYER_PERSON_ADDR_COUNTRY = $customerData['PAYER_PERSON_ADDR_COUNTRY'] ?? null;
                 $customer->PAYER_PERSON_PHONE = $customerData['PAYER_PERSON_PHONE'] ?? null;
                 $customer->PAYER_COMPANY_NAME = $customerData['PAYER_COMPANY_NAME'] ?? null;
 
