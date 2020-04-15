62012db34ab1bdcaeb63c85770218320bf7ac384
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-15 13:17:08 +0300

    doc: add dockblocks to methods

--- src/Product.php
+++ src/Product.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Paytrail\E2Module;
 
+/**
+ * Product class for holding product details for payment.
+ *
+ * @package E2-Module
+ * @author Paytrail <tech@paytrail.com>
+ */
 class Product
 {
     const TYPE_NORMAL = 1;
@@ -11,23 +17,24 @@ class Product
     const TYPE_HANDLING_COST = 3;
 
     public $ITEM_TITLE;
-    public $ITEM_ID;
-    public $ITEM_QUANTITY;
     public $ITEM_UNIT_PRICE;
+    public $ITEM_QUANTITY;
     public $ITEM_TYPE;
-    public $ITEM_DISCOUNT_PERCENT;
     public $ITEM_VAT_PERCENT;
+    public $ITEM_DISCOUNT_PERCENT;
+    public $ITEM_ID;
 
     public static function create(array $productData)
     {
         $product = new Self();
         $product->ITEM_TITLE = $productData['ITEM_TITLE'];
-        $product->ITEM_ID = $productData['ITEM_ID'] ?? null;
         $product->ITEM_UNIT_PRICE = $productData['ITEM_UNIT_PRICE'];
         $product->ITEM_QUANTITY = $productData['ITEM_QUANTITY'] ?? 1;
         $product->ITEM_TYPE = $productData['ITEM_TYPE'] ?? self::TYPE_NORMAL;
+        $product->ITEM_VAT_PERCENT = $productData['ITEM_VAT_PERCENT'] ?? 24;
         $product->ITEM_DISCOUNT_PERCENT = $productData['ITEM_DISCOUNT_PERCENT'] ?? null;
-        $product->ITEM_VAT_PERCENT = $productData['ITEM_VAT_PERCENT'] ?? null;
+        $product->ITEM_ID = $productData['ITEM_ID'] ?? null;
+        
 
         return $product;
     }
