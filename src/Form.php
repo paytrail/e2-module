c2ce800d19045145194dd088d0cd2a02138c886d
Juho-Pekka Vuorela <vuorelajp@gmail.com>
2020-04-15 13:17:08 +0300

    doc: add dockblocks to methods

--- src/Form.php
+++ src/Form.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace Paytrail\E2Module;
 
+/**
+ * Class for creating payment form for payment page.
+ *
+ * @package E2-Module
+ * @author Paytrail <tech@paytrail.com>
+ */
 class Form
 {
     const DEFAULT_FORM_ID = 'PaytrailPaymentForm';
+    const WIDGET_URL = 'https://payment.paytrail.com/js/payment-widget-v1.0.min.js';
 
+    /**
+     * Create Paytrail payment form with widget.
+     *
+     * @param array $paymentData
+     * @param string $buttonText
+     * @param string $formId
+     * @param string|null $widgetUrl
+     * @return string
+     */
     public static function createPaymentWidget(array $paymentData, string $buttonText, string $formId, ?string $widgetUrl): string
     {
-        $formData = "<form action=\"https://payment.paytrail.com/e2\" id=\"{$formId}\">";
+        $formData = '<form action="' . E2Payment::PAYMENT_URL . '" id="' . $formId . '">';
         foreach ($paymentData as $key => $value) {
             $formData .= "<input name=\"{$key}\" type=\"hidden\" value=\"{$value}\">\n";
         }
@@ -23,15 +39,22 @@ class Form
         return $formData;
     }
 
+    /**
+     * Create payment form with button to Paytrail payment page.
+     *
+     * @param array $paymentData
+     * @param string $buttonText
+     * @param string $formId
+     * @return string
+     */
     public static function createPaymentForm(array $paymentData, string $buttonText, string $formId): string
     {
-        $formData = "<form action=\"https://payment.paytrail.com/e2\"  id=\"{$formId}\">";
+        $formData = '<form action="' . E2Payment::PAYMENT_URL . '" id="' . $formId . '">';
         foreach ($paymentData as $key => $value) {
             $formData .= "<input name=\"{$key}\" type=\"hidden\" value=\"{$value}\">\n";
         }
 
-        $formData .= "SV.widget.initWithForm('{$formId}', {charset:'UTF-8'});</script>";
-
+        $formData .= "<input type=\"submit\" value=\"{$buttonText}\"></form>";
 
         return $formData;
     }
